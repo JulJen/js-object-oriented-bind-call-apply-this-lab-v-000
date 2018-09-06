941794615ebe20869500328021c9d8041133f9c8
Jul Jen <juliajen.prog@gmail.com>
2018-09-06 19:53:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,14 @@
 
 function justInvoke(value) {
   return value()
-};
+}
 
 
 // setThisWithCall(fn, thisValue, arg): The function again invokes the function passed to it, but uses the call method to return the function's this value. (Make sure to correctly pass the third argument!)
 
+function setThisWithCall() {
 
+}
 // setThisWithApply(fn, thisValue, args): Again, invoke the function passed to it, change the this value of that function passed to it. In addition, we ask you to invoked the passed function with arguments. You should accomplish all of the above by using apply.
 
 
